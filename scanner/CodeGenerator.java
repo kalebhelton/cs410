c94fff62bc0f0dd64df70aa456ce252fb428102b
kalebhelton <kah00028@mix.wvu.edu>
2024-12-02 13:24:25 -0500

    updated the CodeGenerator -> working on mode displacement

--- scanner/CodeGenerator.java
+++ scanner/CodeGenerator.java
@@ -14,31 +14,123 @@ public class CodeGenerator {
 
     private static byte[] translateAtomToMachineCode(AtomOperation atom) {
         int opcode = getOpcode(atom.getOp());
-        int mode = 0; // Absolute mode
+        int mode = atom.isDisplacementMode() ? 1 : 0; // Displacement mode
         int cmp = 0; // Default comparison
-        int r = 0; // Default register
+        int r = Integer.parseInt(atom.getResult()); // Default register
         int a = 0; // Default address
+        int rx = 0;  // For Displacement Mode
+        int ry = 0;  // For Displacement Mode
+        int d = 0;  // For Displacement Mode
 
         switch (atom.getOp()) {
             case CLR:
-                return encodeInstruction(opcode, mode, cmp, r, 0);
+                return encodeInstruction(opcode, mode, cmp, r, a);
+
             case ADD:
-                return encodeInstruction(opcode, mode, cmp, r, Integer.parseInt(atom.getRight()));
+                if (mode == 0){  // Absolute Mode
+                    a = Integer.parseInt(atom.getRight());
+                    return encodeInstruction(opcode, mode, cmp, r, a);
+                }
+
+                else{  // Displacement Mode
+                    rx = Integer.parseInt(atom.getResult());
+                    ry = Integer.parseInt(atom.getLeft());
+                    d = Integer.parseInt(atom.getRight());
+                    return encodeInstruction(opcode, mode, cmp, rx, ry, d);
+                }
+               
             case SUB:
-                return encodeInstruction(opcode, mode, cmp, r, Integer.parseInt(atom.getRight()));
+                if (mode == 0){
+                    a = Integer.parseInt(atom.getRight());
+                    return encodeInstruction(opcode, mode, cmp, r, a);
+                }
+
+                else{
+                    rx = Integer.parseInt(atom.getResult());
+                    ry = Integer.parseInt(atom.getLeft());
+                    d = Integer.parseInt(atom.getRight());
+                    return encodeInstruction(opcode, mode, cmp, rx, ry, d);
+                }
+                
             case MUL:
-                return encodeInstruction(opcode, mode, cmp, r, Integer.parseInt(atom.getRight()));
+                if (mode == 0){
+                    a = Integer.parseInt(atom.getRight());
+                    return encodeInstruction(opcode, mode, cmp, r, a);
+                }
+
+                else{
+                    rx = Integer.parseInt(atom.getResult());
+                    ry = Integer.parseInt(atom.getLeft());
+                    d = Integer.parseInt(atom.getRight());
+                    return encodeInstruction(opcode, mode, cmp, rx, ry, d);
+                }
+               
             case DIV:
-                return encodeInstruction(opcode, mode, cmp, r, Integer.parseInt(atom.getRight()));
+                if (mode == 0){
+                    a = Integer.parseInt(atom.getRight());
+                    return encodeInstruction(opcode, mode, cmp, r, a);
+                }
+
+                else{
+                    rx = Integer.parseInt(atom.getResult());
+                    ry = Integer.parseInt(atom.getLeft());
+                    d = Integer.parseInt(atom.getRight());
+                    return encodeInstruction(opcode, mode, cmp, rx, ry, d);
+                }
+
             case JMP:
-                return encodeInstruction(opcode, mode, cmp, r, Integer.parseInt(atom.getDest()));
+                if (mode == 0){
+                        a = Integer.parseInt(atom.getRight());
+                        return encodeInstruction(opcode, mode, cmp, r, a);
+                    }
+
+                    else{
+                        rx = Integer.parseInt(atom.getResult());
+                        ry = Integer.parseInt(atom.getLeft());
+                        d = Integer.parseInt(atom.getRight());
+                        return encodeInstruction(opcode, mode, cmp, rx, ry, d);
+                    }
+
             case CMP:
                 cmp = getComparison(atom.getCmp());
-                return encodeInstruction(opcode, mode, cmp, r, Integer.parseInt(atom.getRight()));
+                if (mode == 0){
+                    a = Integer.parseInt(atom.getRight());
+                    return encodeInstruction(opcode, mode, cmp, r, a);
+                }
+
+                else{
+                    rx = Integer.parseInt(atom.getResult());
+                    ry = Integer.parseInt(atom.getLeft());
+                    d = Integer.parseInt(atom.getRight());
+                    return encodeInstruction(opcode, mode, cmp, rx, ry, d);
+                }
+
             case LOD:
-                return encodeInstruction(opcode, mode, cmp, r, Integer.parseInt(atom.getRight()));
+                if (mode == 0){
+                    a = Integer.parseInt(atom.getRight());
+                    return encodeInstruction(opcode, mode, cmp, r, a);
+                }
+
+                else{
+                    rx = Integer.parseInt(atom.getResult());
+                    ry = Integer.parseInt(atom.getLeft());
+                    d = Integer.parseInt(atom.getRight());
+                    return encodeInstruction(opcode, mode, cmp, rx, ry, d);
+                }
+
             case STO:
-                return encodeInstruction(opcode, mode, cmp, r, Integer.parseInt(atom.getRight()));
+                if (mode == 0){
+                    a = Integer.parseInt(atom.getRight());
+                    return encodeInstruction(opcode, mode, cmp, r, a);
+                }
+
+                else{
+                    rx = Integer.parseInt(atom.getResult());
+                    ry = Integer.parseInt(atom.getLeft());
+                    d = Integer.parseInt(atom.getRight());
+                    return encodeInstruction(opcode, mode, cmp, rx, ry, d);
+                }
+
             case HLT:
                 return encodeInstruction(opcode, mode, cmp, r, 0);
             default:
@@ -68,16 +160,27 @@ public class CodeGenerator {
             case "equal": return 1;
             case "lesser": return 2;
             case "greater": return 3;
-            // case "lesser or equal": return 4;
-            // case "greater or equal": return 5;
-            // case "unequal": return 6;
+            case "lesser or equal": return 4;
+            case "greater or equal": return 5;
+            case "unequal": return 6;
             default: throw new IllegalArgumentException("Unknown comparison: " + cmp);
         }
     }
 
-    // Translates the instruction to a byte array
-    private static byte[] encodeInstruction(int opcode, int mode, int cmp, int r, int a) {
+    // Translates the Instruction to a Byte Array -> Absolute Mode
+    private static byte[] encodeInstruction(int opcode, int mode, int cmp, int r, int a){
         int instruction = (opcode << 28) | (mode << 27) | (cmp << 24) | (r << 20) | a;
+        return new byte[] {
+            (byte) (instruction >> 24), 
+            (byte) (instruction >> 16), 
+            (byte) (instruction >> 8),
+            (byte) instruction
+        };
+    }
+
+    // Translates the instruction to a byte array for Displacement Mode
+    private static byte[] encodeInstruction(int opcode, int mode, int cmp, int rx, int ry, int d) {
+        int instruction = (opcode << 28) | (mode << 27) | (cmp << 24) | (rx << 16) | (ry << 12) | d;
         return new byte[] {
             (byte) (instruction >> 24),
             (byte) (instruction >> 16),
